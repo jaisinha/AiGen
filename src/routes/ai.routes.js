be3db9fb18beb5b4fbe2f710bd9762773716bebf
Jai Sinha <72357426+jaisinha@users.noreply.github.com>
2025-05-26 18:27:32 +0300

    Delete src directory

--- src/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-import aiController from "../controllers/ai.controller.js";
-
-router.post("/get-review", aiController.getReview);
-
-export default router;
