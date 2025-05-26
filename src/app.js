1f1e3301a8006a2b34098bf2d96d155cea103c15
Jai Sinha <72357426+jaisinha@users.noreply.github.com>
2025-05-26 17:40:08 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from "express";
 import aiRoutes from "./routes/ai.routes.js";
 import cors from "cors";
+
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://ai-gen-jbps.vercel.app/",  // Replace with your actual origin or use a function for dynamic origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+  credentials: true, // Enable cookies and credentials if needed
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello there");
 });
 
 app.use("/ai", aiRoutes);
+
 export { app };
