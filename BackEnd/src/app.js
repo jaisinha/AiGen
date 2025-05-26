d08030baa40c97547776eb208698bf63d33a842b
Jai Sinha <72357426+jaisinha@users.noreply.github.com>
2025-05-26 18:47:54 +0300

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -2,9 +2,17 @@ import express from "express";
 import aiRoutes from "./routes/ai.routes.js";
 import cors from "cors";
 const app = express();
-app.use(cors());
 import { config } from "dotenv";
 config();
+const corsOptions = {
+  origin: "https://ai-gen-jbps.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"], // Add allowed headers explicitly if needed
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, some Android browsers) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello there");
